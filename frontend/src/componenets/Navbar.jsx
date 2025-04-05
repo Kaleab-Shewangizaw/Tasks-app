dcac5c8f32f2b43b708e94118b9b1e614de3968b
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-04-05 23:42:37 +0300

    search bar done

--- frontend/src/componenets/Navbar.jsx
+++ frontend/src/componenets/Navbar.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import ProfileInfo from './cards/ProfileInfo'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from "react";
+import ProfileInfo from "./cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
-  const navigate = useNavigate()
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
   const onLogout = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
+  const handleSearch = () => {};
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow-md'>
-      <h1 className='text-xl font-extrabold text-black py-2'>Tasks</h1>
-      <ProfileInfo onLogout={onLogout}/>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow-md">
+      <h1 className="text-xl font-extrabold text-black py-2">Tasks</h1>
+      <SearchBar
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+      <ProfileInfo onLogout={onLogout} />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
